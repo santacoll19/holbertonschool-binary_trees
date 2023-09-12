a89f4a17628cbf3420c9a2ef59c56209e47afe4c
santacoll19 <collazokel19@gmail.com>
2023-09-12 14:31:46 -0400

    testing

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,5 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
-
-#include <stddef.h>
+#ifndef BINARY_TREES_H_
+#define BINARY_TREES_H_
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -11,24 +9,17 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
-
-void binary_tree_print(const binary_tree_t *);
-
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
 
 typedef struct binary_tree_s binary_tree_t;
 
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
-
-/*Prototypes*/
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);

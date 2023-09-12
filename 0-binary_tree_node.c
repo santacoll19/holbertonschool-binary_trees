b2cf5568288136347a80d3fbb3b24a157e7397a8
santacoll19 <collazokel19@gmail.com>
2023-09-12 14:34:01 -0400

    fixing

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,8 +12,8 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
   binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-  
-  if (new_node == NULL) 
+
+  if (new_node == NULL)
   {
     return (NULL);
   }

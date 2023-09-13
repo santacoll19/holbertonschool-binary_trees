be460a86ccf3388983dd9fbfb52430aba4aed087
santacoll19 <collazokel19@gmail.com>
2023-09-13 04:29:47 +0000

    testing

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,9 @@
-#ifndef BINARY_TREES_H_
-#define BINARY_TREES_H_
-
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdbool.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -11,17 +14,22 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+  int n;
+  struct binary_tree_s *parent;
+  struct binary_tree_s *left;
+  struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);

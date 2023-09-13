aa103d63d20491b0f8c2abe7d92af0054a0e3cde
santacoll19 <collazokel19@gmail.com>
2023-09-13 00:57:38 +0000

    testing

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -31,7 +31,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
-		new_node->right->parent = new_node;
+		parent->right->parent = new_node;
 	}
 
 

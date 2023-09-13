d6dcf617d8edc9044f016e1856a9252b04d02d14
santacoll19 <collazokel19@gmail.com>
2023-09-13 04:54:58 +0000

    testing

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -11,12 +11,8 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
     if (tree == NULL || func == NULL)
         return;
 
-    // Recursively traverse the left subtree
     binary_tree_postorder(tree->left, func);
-
-    // Recursively traverse the right subtree
     binary_tree_postorder(tree->right, func);
 
-    // Call the function on the current node
     func(tree->n);
 }
